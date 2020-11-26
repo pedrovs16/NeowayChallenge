4685cfad3092702cd526cee075c681870ce43edf
Pedro Viana <pedrovs.contato@gmail.com>
2020-11-26 15:32:46 -0300

    Commit init

---
+++  executable/Interfacee.py
@@ -0,0 +1,68 @@
+import PySimpleGUI as sg
+import functions as ft
+
+
+
+class Interface:
+    def __init__(self):
+
+        # Layout
+
+        sg.theme('dark teal 9')
+        layout = [
+            # Linha 1
+            [sg.Text('=' * 120)],
+            # Linha 2
+            [sg.Text(" " * 40), sg.Text("COMPILADOR DE CEP", )],
+            # Linha 3
+            [sg.Text("Escolha os estado(s):"), sg.Checkbox('AC', key='AC'), sg.Checkbox('AL', key='AL')
+                , sg.Checkbox('AP', key='AP'), sg.Checkbox('AM', key='AM')
+                , sg.Checkbox('BA', key='BA'), sg.Checkbox('CE', key='CE')
+                , sg.Checkbox('DF', key='DF')],
+            # Linha 4
+            [sg.Checkbox('ES  ', key='ES'), sg.Checkbox('GO  ', key='GO')
+                , sg.Checkbox('MA  ', key='MA'), sg.Checkbox('MG  ', key='MG')
+                , sg.Checkbox('MS  ', key='MS'), sg.Checkbox('MT  ', key='MT')
+                , sg.Checkbox('PA  ', key='PA'), sg.Checkbox('PB  ', key='PB')],
+            # Linha 5
+            [sg.Checkbox('PR  ', key='PR'), sg.Checkbox('PE  ', key='PE')
+                , sg.Checkbox('PI  ', key='PI'), sg.Checkbox('RJ  ', key='RJ')
+                , sg.Checkbox('RN  ', key='RN'), sg.Checkbox('RS  ', key='RS')
+                , sg.Checkbox('RO  ', key='RO'), sg.Checkbox('RR  ', key='RR')],
+            # Linha 6
+            [sg.Checkbox('SC  ', key='SC'), sg.Checkbox('SP  ', key='SP')
+                , sg.Checkbox('SE  ', key='SE'), sg.Checkbox('TO  ', key='TO')],
+            # Linha 7
+            [sg.Text('Nome para o arquivo JSONL:'), sg.Input(key='arquivonome')],
+            [sg.Button('Compilar', key='compilar')],
+            # Linha 8
+            [sg.Output(size=(71, 28))],
+        ]
+
+        # JANELA
+
+        window = sg.Window('Neoway', size=(550, 620)).layout(layout)
+
+
+        # Analisando estados escolhidos
+
+        while True:
+            num = 1
+            try:
+                event, values = window.read()
+                if event == sg.WINDOW_CLOSED:  # Se o X da interface for clicado finalizar o programa
+                    break
+                elif event == 'compilar':
+                    for uf in values:
+                        if values[uf] is True:
+                            print(f'----------------Pegando dados de {uf}----------------')
+
+                            num = ft.StateChoice(uf, num, values['arquivonome'])
+                print('JSON formado.')
+
+            except Exception as error:
+                print(error)
+
+
+
+interface = Interface()
---
+++  executable/functions.py
@@ -0,0 +1,135 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import json
+
+
+def StateChoice(uf, num, archiveName):
+    # Para deixar o navegador invisível
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+
+    # Abrindo o navegador
+    try:
+        driver = webdriver.Chrome('chromedriver.exe', options=options)
+    except:
+        print('Não foi possível acessar o site, verifique no seu navegador se o site está '
+              'funcionando http://www.buscacep.correios.com.br/sistemas/buscacep/buscaFaixaCep.cfm.')
+        return num
+
+    # Entrando na página principal
+
+    try:
+        driver.get('http://www.buscacep.correios.com.br/sistemas/buscacep/buscaFaixaCep.cfm')
+    except:
+        print('Não foi possível acessar o site, verifique no seu navegador se o site está '
+              'funcionando http://www.buscacep.correios.com.br/sistemas/buscacep/buscaFaixaCep.cfm.')
+        return num
+
+    # Escolhendo o estado
+    try:
+        if uf == 'RR':  # Se for digitado 'RR' pega as infos de 'RN' e se for 'RRRR' vai para 'RR'
+            search = driver.find_element_by_class_name('f1col')
+            search.send_keys('RRRR')
+            search1 = driver.find_element_by_class_name('f6col')
+            search1.send_keys(Keys.ENTER)
+        else:
+            search = driver.find_element_by_class_name('f1col')
+            search.send_keys(uf)
+            search1 = driver.find_element_by_class_name('f6col')
+            search1.send_keys(Keys.ENTER)
+    except:
+        print('Não foi possível acessar o site, verifique no seu navegador se o site está '
+              'funcionando http://www.buscacep.correios.com.br/sistemas/buscacep/buscaFaixaCep.cfm.')
+        return num
+
+    # Variáveis
+
+    lists = []
+    tmptabela = 2
+
+    dictionary = {'uf': uf, 'id': '', 'localidade': '', 'faixa-de-cep': ''}
+
+    # Pegando os dados da página
+
+    print('Buscando dados...')
+    while True:
+        try:
+
+            # Selecionando a área com os dados
+
+            info = WebDriverWait(driver, 1000).until(
+                EC.presence_of_element_located((By.XPATH, f"/html/body/div/div/div/div/div/div/div/"
+                                                          f"div/table[{tmptabela}]/tbody"))
+            )
+        except:
+            driver.quit()
+            break
+
+        tmptabela = 1
+
+        # Modelando os dados para serem manipulados
+
+        info = info.text
+        info = info.split('\n')
+
+        conditional = False
+        for line in info[1:]:
+            line = line[0:(line.find('-0') + 16)]
+            line = line.replace(' a ', ';')
+            line = line.replace(' 0', '##0')
+            line = line.replace(' 1', '##1')
+            line = line.replace(' 2', '##2')
+            line = line.replace(' 3', '##3')
+            line = line.replace(' 4', '##4')
+            line = line.replace(' 5', '##5')
+            line = line.replace(' 6', '##6')
+            line = line.replace(' 7', '##7')
+            line = line.replace(' 8', '##8')
+            line = line.replace(' 9', '##9')
+            line = line.replace(';', ' a ')
+            line = line.split('##')
+
+            # Adicionando ID
+            if uf == 'DF':  # No distrito federal registra 2 Brasilias diferente com CEP DIFERENTES
+                for county in lists:
+                    if county[1] == line[0] and county[2] == line[1]:
+                        conditional = True
+            else:
+                for county in lists:
+                    if county[1] == line[0]:
+                        conditional = True
+            if not conditional:
+                line.insert(0, num)
+                num += 1
+                lists.append(line)
+                dictionary['localidade'] = line[1]
+                dictionary['id'] = line[0]
+                dictionary['faixa-de-cep'] = line[2]
+
+                # Criando arquivo JSONL
+                try:
+                    archive = open(f'{archiveName}.jsonl', 'rt')
+                    archive.close()
+                except FileNotFoundError:
+                    with open(f'{archiveName}.jsonl', 'w') as f:
+                        json.dump(dictionary, f)
+                        f.write('\n')
+                else:
+                    with open(f'{archiveName}.jsonl', 'at') as f:
+                        json.dump(dictionary, f)
+                        f.write('\n')
+                print(dictionary)
+            conditional = False
+        try:
+
+            # Trocar de página
+
+            page = driver.find_element_by_xpath("/html/body/div/div/div/div/div/div/div/div/div[2]/a")
+            page.click()
+        except:  # Quando não conseguir mais trocar de página cancela a função
+            driver.quit()
+            return num
